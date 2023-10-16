41bb285ae6e11bee871294d70d801bbbf98bbaff
Xuty <xutyxd@gmail.com>
2023-10-16 19:14:14 +0200

    feat(export/doc): Update export adding file-agents and update README.md

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from 'file-agents';
 import { FilePeer } from "./classes/file-peer.class";
 import { FilePortal } from "./classes/file-portal.class";
 import { FileTunnel } from "./classes/file-tunnel.class";
