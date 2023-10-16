cba4bba7b1a17580e6b91591623cc69de3d06303
Xuty <xutyxd@gmail.com>
2023-10-16 19:33:35 +0200

    feat(export): Fix exports for mjs

--- mjs-formatter.sh
+++ mjs-formatter.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 folder_path="mjs"
-extension="class.js"
+extension=".js"
 
 # Find all .js files in the folder and its subdirectories
 js_files=$(find "$folder_path" -type f -name "*$extension")
 
 for file in $js_files; do
-    # Use sed to replace lines ending with ".class" with ".class.js"
-    sed -i 's/\.class\b/\.class.js/g' "$file"
+    echo $file
+    # Use sed to replace ".class" with ".class.js" only on lines starting with "import"
+    sed -i '/^import/s/\.class\b/\.class.js/g' "$file"
 done
+
+echo "Replacement completed."
